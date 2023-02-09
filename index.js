e46d692838a1b8d3ad25e0ccb4c076d79c6c8c93
jr-philip <philipmwaura48@gmail.com>
2023-02-08 19:33:29 -0600

    textarea

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 import { tweetsData } from "./data.js"  //how to receive arrays drom data.js
+
+const tweetBtn = document.getElementById("tweet-btn")
+const tweetInput = document.getElementById("tweet-input")
+
+tweetBtn.addEventListener("click", function(){
+    console.log(tweetInput.value)
+    tweetInput.value = " "
+})
