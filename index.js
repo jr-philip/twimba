5e8075718c455421bfcb0003dc3c8df7abe42ee8
jr-philip <philipmwaura48@gmail.com>
2023-02-10 19:22:17 -0600

    for of

--- index.js
+++ index.js
@@ -9,5 +9,31 @@ tweetBtn.addEventListener("click", function(){
 })
 
 function getFeedHtml(){
-
+    let feedHtml = ''
+    for (let tweet of tweetsData){
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.twiges}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    ${tweet.replies.length} 
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+        `
+    }
+    console.log(feedHtml)
 }
+getFeedHtml()
